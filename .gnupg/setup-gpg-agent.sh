57b85f3a4a8a81adca570f8192f4ed7ad02154f6
Raymond Cheng <me@raymondcheng.net>
2018-12-02 13:17:11 -0800

    key import

--- .gnupg/setup-gpg-agent.sh
+++ .gnupg/setup-gpg-agent.sh
@@ -11,6 +11,16 @@ KEY_FILE="A54149F9DE1419A939455E6ECAF11E602D6FD561-75F74E2556BC1232.asc"
 KEY_URL="https://raymondcheng.net/download/pgp/A54149F9DE1419A939455E6ECAF11E602D6FD561-75F74E2556BC1232.asc"
 AUTH_KEY_ID="75F74E2556BC1232"
 
+URL_PATH="https://raymondcheng.net/download/pgp"
+MASTER_KEY="A54149F9DE1419A939455E6ECAF11E602D6FD561"
+AUTHENTICATE_KEY="75F74E2556BC1232"
+ENCRYPT_KEY="8B00F38A32202B98"
+SIGN_KEY="9E54F484C328ACE5"
+AUTHENTICATE_KEY_URL="$URL_PATH/$MASTER_KEY-$AUTHENTICATE_KEY-authenticate.asc"
+ENCRYPT_KEY_URL="$URL_PATH/$MASTER_KEY-$ENCRYPT_KEY-encrypt.asc"
+SIGN_KEY_URL="$URL_PATH/$MASTER_KEY-$SIGN_KEY-sign.asc"
+
+
 # Create directory
 if [ -d $HOME/.gnupg ]; then echo "~/.gnupg exists"
 else mkdir -p $HOME/.gnupg
@@ -22,9 +32,13 @@ cd $HOME/.gnupg
 keys=`gpg -k`
 if [ ! -z "$keys" -a "$keys" != " " ]; then echo "1. Keys already imported"
 else
-  echo "1. Importing keys from $KEY_URL"
-  curl $KEY_URL > $KEY_FILE
-  gpg --import < $KEY_FILE
+  echo "1. Importing keys from $URL_PATH"
+  curl $AUTHENTICATE_KEY_URL > "$AUTHENTICATE_KEY.asc"
+  curl $ENCRYPT_KEY_URL > "$ENCRYPT_KEY.asc"
+  curl $SIGN_KEY_URL > "$SIGN_KEY.asc"
+  gpg --import < "$AUTHENTICATE_KEY.asc"
+  gpg --import < "$ENCRYPT_KEY.asc"
+  gpg --import < "$SIGN_KEY.asc"
 fi
 
 # Write gpg-agent.conf
