074f48d689c1a1bdbba36c26dad457083849a744
Raymond Cheng <me@raymondcheng.net>
2023-09-20 07:44:09 -0700

    add code-server

--- docker/shell.sh
+++ docker/shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t dev ~/Downloads/code/docker/. && docker run --rm -p 8000:8000 -v `pwd`:/app -it dev
+docker build -t dev ./. && docker run --rm -p 8000:8000 -v `pwd`:/app -it dev
